903d9dfa80b090f96f7986392c7be13f207cfc03
Zaw Htut <zawhtut0010zha@gmail.com>
2025-11-10 11:06:49 +0630

    add develop controller into develop branch

---
+++ BMS.Web/Controllers/DevelopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BMS.Web.Controllers
+{
+    public class DevelopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
